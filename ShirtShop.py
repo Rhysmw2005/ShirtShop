55d3e440d35efc8e7807871f46e9eae71d69b92b
Rhys <williamsr157@cardiff.ac.uk>
2024-05-07 15:17:12 +0100

    Final changes

--- ShirtShop.py
+++ ShirtShop.py
@@ -243,32 +243,6 @@ def home():
     
     return render_template("home.html", shirts=shirts, sort_field=sort_field, sort_order=sort_order)
 
-
-
-# Other routes for the application
-@app.route("/about")
-def about():
-    return render_template("about.html", title="About")
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f"Account successfully created for {form.username.data}!", "success")
-        return redirect(url_for("home"))
-    return render_template("register.html", title="Register", form=form)
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == "admin@a.com" and form.password.data == "123":
-            flash("You are now logged in", "success")
-            return redirect(url_for("home"))
-        else:
-            flash("Login unsuccessful. Please check email and password.", "danger")
-    return render_template("login.html", title="Login", form=form)
-
 # Run the Flask application
 if __name__ == "__main__":
     app.run(debug=True)
